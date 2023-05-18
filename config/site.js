8a7ea281952576b11b40e6c19aee5c8c7d058017
mkubdev <mkubdev@gmail.com>
2023-05-18 17:09:38 +0200

    chore: update projects configuration

---
+++ config/site.js
@@ -0,0 +1,21 @@
+
+export const siteConfig = {
+    name: "mkubdev/KubeXperience",
+    description:
+        "From Zero to Cube Hero: A Three.js Learning Journey.",
+    mainNav: [
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "Xperience",
+            href: "/xperience",
+        }
+    ],
+    links: {
+        xperiment: '/xperience',
+        twitter: "https://twitter.com/mkubdev",
+        github: "https://github.com/mkubdev/kubexperience",
+    },
+}
